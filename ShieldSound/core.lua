c8a9cbae300ca81d233320af2dcc0f0809e3f5d8
Denis Skvortsov <catstrategist@rambler.ru>
2022-02-06 20:30:56 +0300

    Update core.lua

--- ShieldSound/core.lua
+++ ShieldSound/core.lua
@@ -1,48 +1,23 @@
-local frame = CreateFrame("FRAME", "FooAddonFrame");
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
-frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
-frame:RegisterEvent("COMBAT_TEXT_UPDATE");
-frame:RegisterEvent("UNIT_COMBAT");
+local playerGUID = UnitGUID("player")
 
-local function eventHandler(self, event, ...)
+local f = CreateFrame("Frame")
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+f:SetScript("OnEvent", function(self, event)
+  self:COMBAT_LOG_EVENT_UNFILTERED(CombatLogGetCurrentEventInfo())
+end)
 
--- print("Hello World! Hello " .. event);
+function f:COMBAT_LOG_EVENT_UNFILTERED(...)
+  local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
+  local spellId, spellName, spellSchool
+  local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
 
-  local timestamp, type, hideCaster,                                                                      -- arg1  to arg3
-      sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags,   -- arg4  to arg11
-      spellId, spellName, spellSchool,                                                                      -- arg12 to arg14
-      amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing = ...             -- arg15 to arg23
+  if subevent == "SWING_DAMAGE" then
+    amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+  elseif subevent == "SPELL_DAMAGE" then
+    spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+  end
 
-  --[[
-    * Note, for this example, you could just use 'local type = select(2, ...)'.  The others are included
-      so that it's clear what's available.
-    * You can also lump all of the arguments into one block (or one really long line):
-
-    local timestamp, type, hideCaster,                                                                      -- arg1  to arg3
-      sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags,   -- arg4  to arg11
-      spellId, spellName, spellSchool,                                                                      -- arg12 to arg14
-      amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing = ...             -- arg15 to arg23
-  ]]
-
-  if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
-    if (type == "SWING_DAMAGE") then
-    	 amount,
-        overkill,
-        school,
-        resisted,
-        blocked,
-        absorbed,
-        critical,
-        glancing,
-        crushing
-                = select(12, ...)
-      if (blocked>0 and destName == UnitName("player")) then -- "== 1" for clarity only.  Not needed.
-       -- PlaySoundFile("Sound\\Item\\Weapons\\Shields\\WoodenShieldBlock3.OGG","Master")
-       
-        PlaySoundFile("Interface\\AddOns\\ShieldSound\\sound.mp3", "Master")
+  if blocked and blocked > 0 and destGUID == playerGUID then
+    PlaySoundFile("Interface\\AddOns\\ShieldSound\\sound.mp3", "Master")
   end
 end
-end
-end
-
-frame:SetScript("OnEvent", eventHandler);

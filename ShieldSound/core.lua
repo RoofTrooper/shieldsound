4ccebc812079aaf1d3f039c03be14af5a705fcca
Denis Skvortsov <catstrategist@rambler.ru>
2024-01-09 07:31:42 +0300

    Update core.lua
    Fixed an issue with completely blocked attacks, ranged and spell attacks.

--- ShieldSound/core.lua
+++ ShieldSound/core.lua
@@ -10,14 +10,20 @@ function f:COMBAT_LOG_EVENT_UNFILTERED(...)
   local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
   local spellId, spellName, spellSchool
   local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
-
-  if subevent == "SWING_DAMAGE" then
-    amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
-  elseif subevent == "SPELL_DAMAGE" then
-    spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
-  end
-
-  if blocked and blocked > 0 and destGUID == playerGUID then
-    PlaySoundFile("Interface\\AddOns\\ShieldSound\\sound.mp3", "Ambience")
+  
+  if destGUID == playerGUID then 
+    if (subevent == "SWING_MISSED") then 
+      missType, isOffHand, amountMissed, critical = select(12,...)
+    elseif (subevent == "RANGE_MISSED") or (subevent =="SPELL_MISSED") then
+      spellId, spellName, critical, missType = select(12,...)  
+    elseif subevent == "SWING_DAMAGE" then
+      amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+    elseif (subevent == "SPELL_DAMAGE") or (subevent == "RANGE_DAMAGE") then
+      spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
+    end
+      if (missType == "BLOCK") or (blocked and blocked > 0) then 
+        PlaySoundFile("Interface\\AddOns\\ShieldSound\\sound.mp3", "Ambience")
+      end
+      missType, blocked = nil
   end
 end
